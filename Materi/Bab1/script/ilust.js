f5bc5aec29a125f6eb5717284ebdd1266ce440c7
Pathricc1234 <abelputra101@gmail.com>
2024-02-11 14:02:51 +0700

    refactor: all code for displaying ilustrasi and latsol

--- Materi/Bab1/script/ilust.js
+++ Materi/Bab1/script/ilust.js
@@ -1,349 +1,92 @@
-function splitContent(section,code){
-    if(section == 1){
-        var getContent = eliminationMap.get(code);
-        var split = getContent.split(".");
-        return split;
-    }
-    if(section == 2){
-        var getContent = graphMap.get(code);
-        var split = getContent.split(".");
-        return split;
-    }
-    if(section == 3){
-        var getContent = gaussMap.get(code);
-        var split = getContent.split(".");
-        return split;
-    }
-    if(section == 4){
-        var getContent = gaussJordanMap.get(code);
-        var split = getContent.split(".");
-        return split;
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    //Elimination Method
-    const firstElim = document.getElementById("first-elim");
-    const secondElim = document.getElementById("second-elim");
-    const thirdElim = document.getElementById("third-elim");
-    const expElim = document.getElementById("exp-elim");
-    const imgElim = document.querySelector(".ilustrasi-metode-eliminasi img")
-    
-    firstElim.style.backgroundColor = '#00ADB5';
-    var cont = splitContent(1,"first");
-    expElim.textContent = cont[0];
-    imgElim.src = cont[1] + "." + "png";
-
-    if (firstElim) {
-        firstElim.addEventListener("click", function() {
-            firstElim.style.backgroundColor = '#00ADB5';
-            secondElim.style.backgroundColor = '#222831';
-            thirdElim.style.backgroundColor = '#222831';
-            var cont = splitContent(1,"first");
-            expElim.textContent = cont[0];
-            imgElim.src = cont[1] + ".png";
-        });
-    } 
-
-    if(secondElim){
-        secondElim.addEventListener("click", function(){
-            firstElim.style.backgroundColor = '#222831';
-            secondElim.style.backgroundColor = '#00ADB5';
-            thirdElim.style.backgroundColor = '#222831';
-            var cont = splitContent(1,"second");
-            expElim.textContent = cont[0];
-            imgElim.src = cont[1] + ".png";
+document.addEventListener("DOMContentLoaded",function(){
+    //Elim Method
+    var elimIlus = document.querySelectorAll('.elim-method li');
+    var dispElim = document.querySelectorAll('.exp-elim li');
+    elimIlus[0].style.backgroundColor = '#00ADB5';
+    dispElim[0].style.display = "flex";
+    for (var i = 0; i < elimIlus.length; i++) {
+        elimIlus[i].addEventListener("click", function() {
+            for (var j = 0; j < elimIlus.length; j++) {
+                elimIlus[j].style.backgroundColor = '#222831';
+            }
+            this.style.backgroundColor = '#00ADB5';
+            var position = Array.from(elimIlus).indexOf(this);
+            for (var k = 0; k < dispElim.length; k++) {
+                dispElim[k].style.display = "none";
+            }
+            dispElim[position].style.display = "flex";
         });
     }
 
-    if(thirdElim){
-        thirdElim.addEventListener("click", function(){
-            firstElim.style.backgroundColor = '#222831';
-            secondElim.style.backgroundColor = '#222831';
-            thirdElim.style.backgroundColor = '#00ADB5';
-            var cont = splitContent(1,"third");
-            expElim.textContent = cont[0];
-            imgElim.src = cont[1] + "." + cont[2];
-        });
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function(){
     //Graph Method
-    const firstGraph = document.getElementById("first-graph");
-    const secondGraph = document.getElementById("second-graph");
-    const thirdGraph = document.getElementById("third-graph");
-    const expGraph = document.getElementById("exp-graph");
-    const imgGraph = document.querySelector(".ilustrasi-metode-grafik img");
-
-    firstGraph.style.backgroundColor = '#00ADB5';
-    var cont = splitContent(2,"first");
-    expGraph.textContent = cont[0];
-    imgGraph.src = cont[1] + ".png";
-
-
-    if(firstGraph){
-        firstGraph.addEventListener("click", function(){
-            firstGraph.style.backgroundColor = '#00ADB5';
-            secondGraph.style.backgroundColor = '#222831';
-            thirdGraph.style.backgroundColor = '#222831';
-            var cont = splitContent(2,"first");
-            expGraph.textContent = cont[0];
-            imgGraph.src = cont[1] + ".png";
+    var graphIlus = document.querySelectorAll('.graph-method li');
+    var dispGraph = document.querySelectorAll('.exp-graph li');
+    graphIlus[0].style.backgroundColor = '#00ADB5';
+    dispGraph[0].style.display = "flex";
+    for(var i = 0; i < graphIlus.length; i++){
+        graphIlus[i].addEventListener("click",function(){
+            for(var j = 0; j < elimIlus.length;j++){
+                graphIlus[j].style.backgroundColor = '#222831';
+            }
+            this.style.backgroundColor = '#00ADB5';
+            var position = Array.from(graphIlus).indexOf(this);
+            for(var k = 0; k < dispGraph.length;k++){
+                dispGraph[k].style.display = "none";
+            }
+            dispGraph[position].style.display = "flex";
         });
     }
 
-    if(secondGraph){
-        secondGraph.addEventListener("click", function(){
-            firstGraph.style.backgroundColor = '#222831';
-            secondGraph.style.backgroundColor = '#00ADB5';
-            thirdGraph.style.backgroundColor = '#222831';
-            var cont = splitContent(2,"second");
-            expGraph.textContent = cont[0];
-            imgGraph.src = cont[1] + ".png";
-        });
-    }
-
-    if(thirdGraph){
-        thirdGraph.addEventListener("click", function(){
-            firstGraph.style.backgroundColor = '#222831';
-            secondGraph.style.backgroundColor = '#222831';
-            thirdGraph.style.backgroundColor = '#00ADB5';
-            var cont = splitContent(2,"third");
-            expGraph.textContent = cont[0];
-            imgGraph.src = cont[1] + ".png";
-        });
-    }
-});
-
-document.addEventListener("DOMContentLoaded",function(){
     //Gauss Method
-    const firstG = document.getElementById("first-g");
-    const secondG = document.getElementById("second-g");
-    const thirdG = document.getElementById("third-g");
-    const fourthG = document.getElementById("fourth-g");
-    const fifthG = document.getElementById("fifth-g");
-    const expG = document.getElementById("exp-eg");
-    const imgG = document.querySelector(".ilustrasi-metode-gauss img");
+    var gaussIlus = document.querySelectorAll('.eg-method li');
+    var dispGauss = document.querySelectorAll('.exp-gauss li');
+    gaussIlus[0].style.backgroundColor = '#00ADB5';
+    dispGauss[0].style.display = "flex";
+    for(var i = 0; i < gaussIlus.length; i++){
+        gaussIlus[i].addEventListener("click",function(){
+            for(var j = 0; j < gaussIlus.length;j++){
+                gaussIlus[j].style.backgroundColor = '#222831';
+            }
+            this.style.backgroundColor = '#00ADB5';
+            var position = Array.from(gaussIlus).indexOf(this);
+            for(var k = 0; k < dispGauss.length;k++){
+                dispGauss[k].style.display = "none";
+            }
+            dispGauss[position].style.display = "flex";
+        });
+    }
 
-    firstG.style.backgroundColor = '#00ADB5';
-    var cont = splitContent(3,"first");
-    expG.textContent = cont[0];
-    imgG.src = cont[1] + ".png";
+    //Gaus Jordan Method
+    var gjIlus = document.querySelectorAll('.egj-method li');
+    var dispGJ = document.querySelectorAll('.exp-gj li');
+    gjIlus[0].style.backgroundColor = '#00ADB5';
+    dispGJ[0].style.display = "flex";
+    for(var i = 0; i < gjIlus.length; i++){
+        gjIlus[i].addEventListener("click",function(){
+            for(var j = 0; j < gjIlus.length;j++){
+                gjIlus[j].style.backgroundColor = '#222831';
+            }
+            this.style.backgroundColor = '#00ADB5';
+            var position = Array.from(gjIlus).indexOf(this);
+            for(var k = 0; k < dispGJ.length;k++){
+                dispGJ[k].style.display = "none";
+            }
+            dispGJ[position].style.display = "flex";
+        });
+    }
 
-    if(firstG){
-        firstG.addEventListener("click",function(){
-            firstG.style.backgroundColor = '#00ADB5';
-            secondG.style.backgroundColor = '#222831';
-            thirdG.style.backgroundColor = '#222831';
-            fourthG.style.backgroundColor = '#222831';
-            fifthG.style.backgroundColor = '#222831';
-            var cont = splitContent(3,"first");
-            expG.textContent = cont[0];
-            imgG.src = cont[1] + ".png";
-        });
-    }
-    if(secondG){
-        secondG.addEventListener("click",function(){
-            firstG.style.backgroundColor = '#222831';
-            secondG.style.backgroundColor = '#00ADB5';
-            thirdG.style.backgroundColor = '#222831';
-            fourthG.style.backgroundColor = '#222831';
-            fifthG.style.backgroundColor = '#222831';
-            var cont = splitContent(3,"second");
-            expG.textContent = cont[0];
-            imgG.src = cont[1] + ".png";
-        });
-    }
-    if(thirdG){
-        thirdG.addEventListener("click",function(){
-            firstG.style.backgroundColor = '#222831';
-            secondG.style.backgroundColor = '#222831';
-            thirdG.style.backgroundColor = '#00ADB5';
-            fourthG.style.backgroundColor = '#222831';
-            fifthG.style.backgroundColor = '#222831';
-            var cont = splitContent(3,"third");
-            expG.textContent = cont[0];
-            imgG.src = cont[1] + ".png";
-        });
-    }
-    if(fourthG){
-        fourthG.addEventListener("click",function(){
-            firstG.style.backgroundColor = '#222831';
-            secondG.style.backgroundColor = '#222831';
-            thirdG.style.backgroundColor = '#222831';
-            fourthG.style.backgroundColor = '#00ADB5';
-            fifthG.style.backgroundColor = '#222831';
-            var cont = splitContent(3,"fourth");
-            expG.textContent = cont[0];
-            imgG.src = cont[1] + ".png";
-        });
-    }
-    if(fifthG){
-        fifthG.addEventListener("click",function(){
-            firstG.style.backgroundColor = '#222831';
-            secondG.style.backgroundColor = '#222831';
-            thirdG.style.backgroundColor = '#222831';
-            fourthG.style.backgroundColor = '#222831';
-            fifthG.style.backgroundColor = '#00ADB5';
-            var cont = splitContent(3,"fifth");
-            expG.textContent = cont[0];
-            imgG.src = cont[1] + ".png";
-        });
-    }
-});
-
-document.addEventListener("DOMContentLoaded",function(){
-    //Gauss Method
-    const firstGJ = document.getElementById("first-gj");
-    const secondGJ = document.getElementById("second-gj");
-    const thirdGJ = document.getElementById("third-gj");
-    const fourthGJ = document.getElementById("fourth-gj");
-    const fifthGJ = document.getElementById("fifth-gj");
-    const sixthGJ = document.getElementById("sixth-gj");
-    const expGJ = document.getElementById("exp-egj");
-    const imgGJ = document.querySelector(".ilustrasi-metode-gauss-jordan img");
-
-    firstGJ.style.backgroundColor = '#00ADB5';
-    var cont = splitContent(4,"first");
-    expGJ.textContent = cont[0];
-    imgGJ.src = cont[1] + ".png";
-
-    if(firstGJ){
-        firstGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#00ADB5';
-            secondGJ.style.backgroundColor = '#222831';
-            thirdGJ.style.backgroundColor = '#222831';
-            fourthGJ.style.backgroundColor = '#222831';
-            fifthGJ.style.backgroundColor = '#222831';
-            sixthGJ.style.backgroundColor = '#222831';
-            var cont = splitContent(4,"first");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-    if(secondGJ){
-        secondGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#222831';
-            secondGJ.style.backgroundColor = '#00ADB5';
-            thirdGJ.style.backgroundColor = '#222831';
-            fourthGJ.style.backgroundColor = '#222831';
-            fifthGJ.style.backgroundColor = '#222831';
-            sixthGJ.style.backgroundColor = '#222831';
-            var cont = splitContent(4,"second");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-    if(thirdGJ){
-        thirdGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#222831';
-            secondGJ.style.backgroundColor = '#222831';
-            thirdGJ.style.backgroundColor = '#00ADB5';
-            fourthGJ.style.backgroundColor = '#222831';
-            fifthGJ.style.backgroundColor = '#222831';
-            sixthGJ.style.backgroundColor = '#222831';
-            var cont = splitContent(4,"third");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-    if(fourthGJ){
-        fourthGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#222831';
-            secondGJ.style.backgroundColor = '#222831';
-            thirdGJ.style.backgroundColor = '#222831';
-            fourthGJ.style.backgroundColor = '#00ADB5';
-            fifthGJ.style.backgroundColor = '#222831';
-            sixthGJ.style.backgroundColor = '#222831';
-            var cont = splitContent(4,"fourth");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-    if(fifthGJ){
-        fifthGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#222831';
-            secondGJ.style.backgroundColor = '#222831';
-            thirdGJ.style.backgroundColor = '#222831';
-            fourthGJ.style.backgroundColor = '#222831';
-            fifthGJ.style.backgroundColor = '#00ADB5';
-            sixthGJ.style.backgroundColor = '#222831';
-            var cont = splitContent(4,"fifth");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-    if(sixthGJ){
-        sixthGJ.addEventListener("click",function(){
-            firstGJ.style.backgroundColor = '#222831';
-            secondGJ.style.backgroundColor = '#222831';
-            thirdGJ.style.backgroundColor = '#222831';
-            fourthGJ.style.backgroundColor = '#222831';
-            fifthGJ.style.backgroundColor = '#222831';
-            sixthGJ.style.backgroundColor = '#00ADB5';
-            var cont = splitContent(4,"sixth");
-            expGJ.textContent = cont[0];
-            imgGJ.src = cont[1] + ".png";
-        });
-    }
-});
-
-document.addEventListener("DOMContentLoaded",function(){
-    const pg1btn = document.getElementById("pem-pg1-btn");
-    const pg1dis = document.getElementById("pg1-dis");
-    const pg2btn = document.getElementById("pem-pg2-btn");
-    const pg2dis = document.getElementById("pg2-dis");
-    const ess1btn = document.getElementById("pem-ess1-btn");
-    const ess1dis = document.getElementById("ess1-dis");
-    const ess2btn = document.getElementById("pem-ess2-btn");
-    const ess2dis = document.getElementById("ess2-dis")
-
-    if(pg1btn){
-        pg1btn.addEventListener("click",function(){
-            if(pg1dis.style.display == "none"){
-                pg1dis.style.display = "flex";
-                pg1btn.style.color = '#00ADB5';
+    var plusBtn = document.querySelectorAll('.soal i');
+    var disSol = document.querySelectorAll('.soal .pembahasan-latsol');
+    for(var i = 0; i < plusBtn.length;i++){
+        plusBtn[i].addEventListener("click",function(){
+            var position = Array.from(plusBtn).indexOf(this);
+            if(disSol[position].style.display == "none"){
+                disSol[position].style.display = "flex";
+                plusBtn[position].style.color = '#00ADB5';
             }
             else{
-                pg1dis.style.display = "none";
-                pg1btn.style.color = '#EEEEEE';
-            }
-        });
-    }
-    if(pg2btn){
-        pg2btn.addEventListener("click",function(){
-            if(pg2dis.style.display == "none"){
-                pg2dis.style.display = "flex";
-                pg2btn.style.color = '#00ADB5';
-            }
-            else{
-                pg2dis.style.display = "none";
-                pg2btn.style.color = '#EEEEEE';
-            }
-        });
-    }
-    if(ess1btn){
-        ess1btn.addEventListener("click",function(){
-            if(ess1dis.style.display == "none"){
-                ess1dis.style.display = "flex";
-                ess1btn.style.color = '#00ADB5';
-            }
-            else{
-                ess1dis.style.display = "none";
-                ess1btn.style.color = '#EEEEEE';
-            }
-        });
-    }
-    if(ess2btn){
-        ess2btn.addEventListener("click",function(){
-            if(ess2dis.style.display == "none"){
-                ess2dis.style.display = "flex";
-                ess2btn.style.color = '#00ADB5';
-            }
-            else{
-                ess2dis.style.display = "none";
-                ess2btn.style.color = '#EEEEEE';
+                disSol[position].style.display = "none";
+                plusBtn[position].style.color = '#EEEEEE';
             }
         });
     }
